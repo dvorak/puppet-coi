eed5b7427908bd3e4a189d1dbbb589db52f8c2e7
Clayton O'Neill <clayton@oneill.net>
2014-04-16 11:09:12 -0700

    Remove NTP and naginator from coi base
    We do this ourselves, so we don't need this anymore

--- manifests/profiles/base.pp
+++ manifests/profiles/base.pp
@@ -2,21 +2,5 @@
 # this class stores base configurations
 # that should be applied to all nodes
 #
-# == Parameters
-#   [ntp_servers]
-#     List of ntp servers to use for time synchronization.
-#
 class coi::profiles::base(
-  $ntp_servers = hiera('ntp_servers'),
-) {
-
-  class { ntp:
-    servers => $ntp_servers,
-  }
-
-  #
-  # TODO I need to look more into this file to ensure
-  # that it should be applied everywhere
-  class { "naginator::base_target": }
-
 }
